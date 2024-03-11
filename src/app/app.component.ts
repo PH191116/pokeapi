68bd630721d9f94e0f9a29fcd56c775ccc36c354
Mike Padilla <PH191116@alumno.udb.edu.sv>
2024-03-11 16:29:23 -0600

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+  <img class="pokemon" [src]="pokemon?.sprites.front_default" alt="{{ pokemon?.name }} image" *ngIf="pokemon">
+    <h1>Name: {{ pokemon?.name }}</h1>
+    
+  `,
+  styles: []
 })
-export class AppComponent {
-  title = 'pokeapi';
+export class AppComponent implements OnInit {
+  pokemon: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://pokeapi.co/api/v2/pokemon/ditto').subscribe(
+      (data: any) => {
+        this.pokemon = data;
+      },
+      (error) => {
+        console.error('Error fetching Pokemon:', error);
+      }
+    );
+  }
 }
